d5cc723ade6dc250c74e1a085852a7a920ccf146
Jamal Boutkabout <jboutkabout@cloudflare.com>
2024-06-03 18:45:29 +1000

    update upload

--- functions/upload.js
+++ functions/upload.js
@@ -24,7 +24,7 @@ export async function onRequest(context) {
         */
         const response = await context.env.AI.run('@cf/openai/whisper', input);
   
-          return Response.json({ input: { audio: [] }, response });
+          return Response.json({ input: { audio: [input.response.text] }, response });
          // return Response.json({input:"kk"})
       } else {
         return new Response('No video file uploaded', { status: 400 });
